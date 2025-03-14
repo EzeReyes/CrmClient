babe6a24672a98f237fe617d5ede15c3d8e09cf3
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2025-03-13 23:28:04 -0300

    ultima

--- config/apollo.js
+++ config/apollo.js
@@ -2,8 +2,7 @@ import { ApolloClient, createHttpLink , InMemoryCache } from "@apollo/client";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-    uri: 'https://crmgraphql-yh68.onrender.com',
-    fetch
+    uri: 'https://crmgraphql-yh68.onrender.com'
 });
 
 const authLink = setContext((_, { headers }) => {
